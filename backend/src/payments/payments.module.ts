82e28058d607096392b40f7389128fad28e4e877
Ichurri <santiagoiturrivargas04@gmail.com>
2025-09-22 22:44:34 -0400

    feat: integrate payments module with TypeORM and order system
    - Add Payment and PaymentLog repositories to PaymentsModule
    - Import Order and Table entities for payment workflow
    - Export PaymentsService for use in OrdersService
    - Configure proper module dependencies for payment processing

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './payment.entity';
 import { PaymentLog } from './payment-log.entity';
+import { Order } from '../orders/order.entity';
+import { Table } from '../tables/table.entity';
+import { PaymentsService } from './payments.service';
+import { PaymentsController } from './payments.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, PaymentLog])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Payment, PaymentLog, Order, Table])],
+  providers: [PaymentsService],
+  controllers: [PaymentsController],
+  exports: [PaymentsService, TypeOrmModule],
 })
 export class PaymentsModule {}
